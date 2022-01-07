a153bc929063abb878431c251d5f1aa24531ec13
Jason Songvilay <JSongvilay@bofifederalbank.com>
2022-01-07 10:44:37 -0800

    initial commit

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json({ text: "Hello" });
 }
+
+/*Do Not Fetch an API Route from getStaticProps or getStaticPaths
+You should not fetch an API Route from getStaticProps or getStaticPaths. Instead, write your server-side code directly in getStaticProps or getStaticPaths (or call a helper function). */
